a252cb41b8c63a9a41524d442ace42d679b660b2
jordantanaliga100 <jordantanaliga100@gmail.com>
2025-08-23 14:31:53 +0800

    feat(loops): added loops for cleaner code

--- analyze-logs.sh
+++ analyze-logs.sh
@@ -22,41 +22,36 @@ echo "===================="
 LOG_FILES=$(find $LOG_DIR -name "*.log" -mtime -1)
 echo "$LOG_FILES"
 
-echo -e "\nsearching "ERROR" logs in application.log file"
-grep "${ERROR_PATTERNS[0]}" $LOG_DIR/$APP_LOG_FILE
+# echo -e "\nsearching "ERROR" logs in application.log file"
+# grep "${ERROR_PATTERNS[0]}" $LOG_DIR/$APP_LOG_FILE
 
-echo -e "\nNumber of error logs found in application.log"
-grep -c "${ERROR_PATTERNS[0]}" $LOG_DIR/$APP_LOG_FILE
+# echo -e "\nNumber of error logs found in application.log"
+# grep -c "${ERROR_PATTERNS[0]}" $LOG_DIR/$APP_LOG_FILE
 
-echo -e "\nNumber of fatal logs found in application.log"
-grep -c "${ERROR_PATTERNS[1]}" $LOG_DIR/$APP_LOG_FILE
+# echo -e "\nNumber of fatal logs found in application.log"
+# grep -c "${ERROR_PATTERNS[1]}" $LOG_DIR/$APP_LOG_FILE
 
-echo -e "\nNumber of fatal logs found in system.log"
-grep -c "${ERROR_PATTERNS[1]}" $LOG_DIR/$SYS_LOG_FILE
+# echo -e "\nNumber of fatal logs found in system.log"
+# grep -c "${ERROR_PATTERNS[1]}" $LOG_DIR/$SYS_LOG_FILE
 
-echo -e "\nNumber of critical logs found in system.log"
-grep -c "${ERROR_PATTERNS[2]}" $LOG_DIR/$SYS_LOG_FILE
+# echo -e "\nNumber of critical logs found in system.log"
+# grep -c "${ERROR_PATTERNS[2]}" $LOG_DIR/$SYS_LOG_FILE
 
-echo -e "\nsearching for critical logs found in system.log"
-grep "${ERROR_PATTERNS[2]}" $LOG_DIR/$SYS_LOG_FILE
+# echo -e "\nsearching for critical logs found in system.log"
+# grep "${ERROR_PATTERNS[2]}" $LOG_DIR/$SYS_LOG_FILE
 
 # loops here...
 for LOG_FILE in $LOG_FILES; do
-  echo -e "\nsearching "ERROR" logs in "$LOG_FILE" file"
-  grep "${ERROR_PATTERNS[0]}" "$LOG_FILE"
+  echo -e "\n"
+  echo "====================================="
+  echo "==============$LOG_FILE=============="
+  echo "====================================="
+  
+  for PATTERN in ${ERROR_PATTERNS[@]}; do
+    echo -e "\nSearching $PATTERN logs in "$LOG_FILE" file"
+    grep "$PATTERN" "$LOG_FILE"
 
-  echo -e "\nNumber of error logs found in "$LOG_FILE""
-  grep -c "${ERROR_PATTERNS[0]}" "$LOG_FILE"
-
-  echo -e "\nNumber of fatal logs found in "$LOG_FILE""
-  grep -c "${ERROR_PATTERNS[1]}" "$LOG_FILE"
-
-  echo -e "\nNumber of fatal logs found in "$LOG_FILE""
-  grep -c "${ERROR_PATTERNS[1]}" "$LOG_FILE"
-
-  echo -e "\nNumber of critical logs found in "$LOG_FILE""
-  grep -c "${ERROR_PATTERNS[2]}" "$LOG_FILE"
-
-  echo -e "\nsearching for critical logs found in "$LOG_FILE""
-  grep "${ERROR_PATTERNS[2]}" "$LOG_FILE"
+    echo -e "\nNumber of "$PATTERN" logs found in "$LOG_FILE""
+    grep -c "$PATTERN" "$LOG_FILE"
+  done
 done
